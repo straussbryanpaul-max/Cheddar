ec0c24a73fbd45bda63b959197aca8f398dc94da
straussbryanpaul-max <straussbryanpaul@gmail.com>
2026-05-09 17:39:02 +0000

    Revert "Remove temporary __cheddarRestore recovery helpers"
    This reverts commit d25e9686d3c891abd46ab087c94a662f67d2e45b.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AuthProvider } from './lib/auth'
+import { installRecoveryHelpers } from './lib/recovery'
+
+installRecoveryHelpers()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
